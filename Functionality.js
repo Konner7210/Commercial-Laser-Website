87e01a9092b9c7570692f307f689346361d6bf27
Konner7210 <konner7210@gmail.com>
2025-03-07 02:32:59 -0600

    Update Functionality.js

--- Functionality.js
+++ Functionality.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const homeButton = document.getElementById('homeButton');
 
+    homeButton.addEventListener('click', (e) => {
+        e.preventDefault(); // Prevent default anchor behavior
+        // Add your desired functionality here
+        document.getElementById('home').scrollIntoView({ behavior: 'smooth' });
+    });
+});
